6b69ee331657c1c1e75a7aae9aed604af388afb2
anaaydin <aydinana6.gmail.com>
2020-05-13 07:49:56 -0700

    add todo component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>پروژه گيت هاب</h1>
-      <h3>تست تغييرات</h3>
-    </div>
-  );
+
+class App extends Component {
+    state={
+        items:[],
+        item:""
+    }
+    handleSubmit=e=>{
+        e.preventDefault();
+        const newItem={
+            title:this.state.item
+        }
+        const updateItems=[...this.state.items,newItem]
+        this.setState(
+            {
+                items:updateItems,
+                item:"",
+            },()=>{
+                console.log(this.state.items)
+            })
+    }
+    handleChange=e=>{
+        this.setState(
+            {
+                item:e.target.value
+            }
+        )
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-10 mx-auto mt-5">
+                        <TodoInput handleChange={this.handleChange} handleSubmit={this.handleSubmit} item={this.state.item} />
+                        <TodoList/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
