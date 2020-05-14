6f62af0d36a62756fdf628e32fb3ed6ca9bb4ab4
anaaydin <aydinana6.gmail.com>
2020-05-14 08:00:02 -0700

    add edit and delete handle

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, {Component} from 'react';
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
     state={
         items:[],
-        item:""
+        item:"",
+        id:uuidv4(),
     }
-    handleSubmit=e=>{
+
+    handleSubmit=(e)=>{
         e.preventDefault();
         const newItem={
+            id:this.state.id,
             title:this.state.item
         }
         const updateItems=[...this.state.items,newItem]
@@ -19,24 +22,31 @@ class App extends Component {
             {
                 items:updateItems,
                 item:"",
+                id:uuidv4()
             },()=>{
                 console.log(this.state.items)
             })
     }
-    handleChange=e=>{
+    handleChange=(e)=>{
         this.setState(
             {
                 item:e.target.value
             }
         )
     }
+    handleEdit=(id)=>{
+        console.log("Edit",id)
+    }
+    handleDelete=(id)=>{
+        console.log("Delete",id)
+    }
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-10 mx-auto mt-5">
-                        <TodoInput handleChange={this.handleChange} handleSubmit={this.handleSubmit} item={this.state.item} />
-                        <TodoList/>
+                        <TodoInput handleChange={this.handleChange} handleSubmit={this.handleSubmit} item={this.state.item}  />
+                        <TodoList items={this.state.items} handleEdit={this.handleEdit} handleDelete={this.handleDelete}/>
                     </div>
                 </div>
             </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,15 @@ import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
     render() {
+        const {items,handleDelete,handleEdit} = this.props
         return (
-            <div>
-                <TodoItem/>
-            </div>
+            <ul className="list-group">
+                {items.map(item => {
+                    return (
+                        <TodoItem key={item.id} title={item.title} handleDelete={()=>handleDelete(item.id)} handleEdit={()=>handleEdit(item.id)}/>
+                    )
+                })}
+            </ul>
         );
     }
 }
